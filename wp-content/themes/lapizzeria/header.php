2126d123703ed75974e1a8ae0189e4d8b4456b0f
deivisjl <deivisjl@gmail.com>
2020-04-15 17:04:06 -0600

    Agregando header

---
+++ wp-content/themes/lapizzeria/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<header class="site-header">
+		<div class="logo">
+			<a href="<?php echo esc_url(home_url('/')); ?>">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logo">
+			</a>
+		</div>
+		<div class="informacion-header">
+			<div class="redes-sociales">
+				<!-- TODO: Agregar menu -->
+			</div><!-- Redes sociales -->
+			<div class="direccion">
+				<p>8179 Bay AVenue Mountain View, CA 94043</p>
+				<p>Teléfono: 998633565</p>
+			</div>
+		</div><!-- .informacion-header -->
+	</header>
+	
--- wp-content/themes/lapizzeria/page.php
+++ wp-content/themes/lapizzeria/page.php
@@ -1,3 +1,5 @@
+<?php get_header(); ?>
+
 <?php while(have_posts()): the_post(); ?>
 	<h3><?php the_title(); ?></h3>
 	<?php the_content(); ?>
