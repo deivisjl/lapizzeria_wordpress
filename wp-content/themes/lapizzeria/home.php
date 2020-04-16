3e269b390fbc75b9ac96ca240ee2aedf569ca22c
deivisjl <deivisjl@gmail.com>
2020-04-16 07:57:11 -0600

    Primeros pasos con el blog

---
+++ wp-content/themes/lapizzeria/home.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+	
+	<?php 
+		$pagina_blog = get_option('page_for_posts');
+		$imagen_id = get_post_thumbnail_id($pagina_blog);
+		$imagen_src = wp_get_attachment_image_src($imagen_id, 'full')[0];
+
+	 ?>
+
+	 <div class="hero" style="background-image: url(<?php echo $imagen_src; ?>)">
+		<div class="contenido-hero">
+			<h1><?php echo get_the_title($pagina_blog); ?></h1>		
+		</div>
+	</div>
+
+	<div class="seccion contenedor">
+		<main class="contenido-principal">
+			<?php while(have_posts()):the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+			<?php endwhile; ?>
+		</main>
+	</div>
+<?php get_footer(); ?>
