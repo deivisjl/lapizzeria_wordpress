f31c7c25f1ad4751b529719b76aa9612853a9bd2
deivisjl <deivisjl@gmail.com>
2020-04-26 08:56:27 -0600

    Consultado la base de datos de comentarios

--- wp-content/themes/lapizzeria/comments.php
+++ wp-content/themes/lapizzeria/comments.php
@@ -5,4 +5,23 @@
 		); 
 		comment_form($args); 
 	?>
+	<h3 class="text-center">Comentarios</h3>
+	<ul class="lista-comentarios">
+		<?php 
+
+			$args = array(
+				'post_id' => $post->ID,
+				'status' => 'approve'
+			);
+
+			$comentarios = get_comments($args);
+
+			$args = array(
+				'per_page' => 10,
+				'reverse_top_level' =>false
+			);
+
+			wp_list_comments($args2, $comentarios);
+		 ?>
+	</ul>
 </div>
